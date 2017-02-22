4a02c27a5eb6bd979565404996b20985fd862310
Emmet O'Grady <emmet789@gmail.com>
2017-02-22 12:51:35 +0100

    Add proxy

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # demo-ui5
-A demo application to demonstrate using the OpenUI5 framework
+A demo application to demonstrate using the OpenUI5 framework.
+
+It uses the Northwind public OData database, this application is
+just a nice frontend for the northwind backend.
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you've got [Docker](https://docs.docker.com/engine/installation/) 
+and [docker-compose](https://docs.docker.com/compose/install/) installed.
+
+### Steps
+
+Download this repo:
+```
+$ git clone https://github.com/emmetog/demo-ui5.git
+```
+
+Then run docker-compose up from within the repo:
+```
+$ cd demo-ui5
+$ docker-compose up
+```
+
+That's it. The first time you run it the image will be built, which 
+might take a few minutes (subsequent builds will be much quicker though).
+After the build finishes, go to this url in your browser:
+
+http://localhost:81
---
+++ web/www/Component.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+    "use strict";
+    return UIComponent.extend("emmetog.demo.ui5.Component", {
+
+        metadata : {
+            manifest: "json"
+        },
+
+
+        init : function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // set data model on view
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+
+            // set i18n model
+            var i18nModel = new ResourceModel({
+                bundleName : "emmetog.demo.ui5.i18n.i18n"
+            });
+            this.setModel(i18nModel, "i18n");
+        }
+    });
+});
